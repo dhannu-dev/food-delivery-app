6105ce6e0bd640ebfbe337e9b6f2e032e7bcce83
dhannu-dev <krdhanak47@gmail.com>
2025-12-23 08:54:15 +0530

    removing useSearchParams

--- src/app/user-auth/page.jsx
+++ src/app/user-auth/page.jsx
@@ -1,14 +1,34 @@
 "use client";
 
-import React, { Suspense } from "react";
-import AuthContent from "./AuthContent";
-
-export const dynamic = "force-dynamic";
+import UserLogin from "@/components/UserLogin";
+import UserSignUp from "@/components/UserSignUp";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
+  const [login, setLogin] = useState(true);
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setOrder(params.get("order"));
+  }, []);
+
   return (
-    <Suspense fallback={<div className="text-white">Loading...</div>}>
-      <AuthContent />
-    </Suspense>
+    <div className="flex justify-center items-center h-screen flex-col">
+      {login ? (
+        <UserLogin redirect={order} />
+      ) : (
+        <UserSignUp redirect={order} />
+      )}
+
+      <div
+        className="mt-2 text-white cursor-pointer"
+        onClick={() => setLogin(!login)}
+      >
+        {login
+          ? "Do not have an account ? SignUp"
+          : "Already have an account ? Login"}
+      </div>
+    </div>
   );
 }
