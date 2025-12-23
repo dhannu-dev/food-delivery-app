1e858c5e53ad9feb018efad2477c7763b472cbf6
dhannu-dev <krdhanak47@gmail.com>
2025-12-23 08:50:48 +0530

    improving ui

--- src/app/user-auth/page.jsx
+++ src/app/user-auth/page.jsx
@@ -1,28 +1,14 @@
 "use client";
-import UserLogin from "@/components/UserLogin";
-import UserSignUp from "@/components/UserSignUp";
-import { useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+
+import React, { Suspense } from "react";
+import AuthContent from "./AuthContent";
+
+export const dynamic = "force-dynamic";
 
 export default function Page() {
-  const [login, setLogin] = useState(true);
-  const searchParams = useSearchParams();
-  const order = searchParams.get("order"); // ✅ SAFE
-
-  console.log("order flag:", order);
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      {login ? <UserLogin  redirect = {order}  /> : <UserSignUp redirect = {order}  />}
-      <div
-        className="mt-2 text-white cursor-pointer"
-        onClick={() => setLogin(!login)}
-      >
-        {login ? (
-          <h1>Do not have an account ? SignUp</h1>
-        ) : (
-          <h1>Already hava an account ? Login</h1>
-        )}
-      </div>
-    </div>
+    <Suspense fallback={<div className="text-white">Loading...</div>}>
+      <AuthContent />
+    </Suspense>
   );
 }
